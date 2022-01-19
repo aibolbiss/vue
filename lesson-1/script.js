1233564612fc6947406a7b39c97118e61760d1f3
aibolbiss <63847083+aibolbiss@users.noreply.github.com>
2022-01-20 03:13:39 +0600

    Add files via upload

---
+++ lesson-1/script.js
@@ -0,0 +1,67 @@
+Vue.createApp({
+  data() {
+    return {
+      firstname: "Aibol",
+      secondname: "Bissenkul",
+      lastname: "Amankosovich",
+      users: [
+        {
+          id: 1,
+          firstname: "Erlan",
+          secondname: "Aubakirov",
+        },
+        {
+          id: 2,
+          firstname: "Azamat",
+          secondname: "Bizhanov",
+        },
+        {
+          id: 3,
+          firstname: "Miras",
+          secondname: "Kaidarov",
+        },
+        {
+          id: 4,
+          firstname: "Zhanerke",
+          secondname: "Kappar",
+        },
+        {
+          id: 5,
+          firstname: "Asemai",
+          secondname: "Abitaeva",
+        },
+      ],
+      page: 1,
+    };
+  },
+  computed: {
+    fullName() {
+      console.log("Вызвался computed");
+      return `${this.firstname} ${this.secondname} ${this.lastname}`.toUpperCase();
+    },
+  },
+  methods: {
+    userFullName() {
+      console.log("Вызвался methods");
+      return `${this.users[this.page - 1].firstname} ${
+        this.users[this.page - 1].secondname
+      }`;
+    },
+    nazad() {
+      console.log("Нажали назад");
+      if (this.page > 1) {
+        return this.page--;
+      }
+    },
+    vpered() {
+      console.log("Нажали вперед");
+      if (this.page < this.users.length) {
+        return this.page++;
+      }
+    },
+    number(index) {
+      console.log(`Нажали на кнопку`);
+      return (this.page = index + 1);
+    },
+  },
+}).mount("#app");
